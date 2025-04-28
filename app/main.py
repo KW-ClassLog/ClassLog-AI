a07f89c359285b5f54eeed463bdbe314fb368b7b
Haemin Kim <haemin1230@gmail.com>
2025-04-28 15:48:38 +0900

    ♻️ (#10) generate-quiz, regenerate-quiz API 재구성

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from app.api.quiz_api import quiz_router
+
+load_dotenv()
+
+app = FastAPI()
+
+app.include_router(quiz_router, prefix="/api/quizzes")
