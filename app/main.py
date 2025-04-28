28536b8fd3f38d77f5aac22be53ed916854ff81d
Haemin Kim <haemin1230@gmail.com>
2025-04-28 16:31:03 +0900

    🔥 (#10) 기존 파일들 삭제

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
-from app.api.quiz_api import quiz_router
-
-load_dotenv()
-
-app = FastAPI()
-
-app.include_router(quiz_router, prefix="/api/quizzes")
